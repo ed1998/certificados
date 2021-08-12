27f5b92eba933f376e79b49d171bec456125e553
Jhon Gutierrez <jhongtrrz@gmail.com>
2021-08-11 21:15:13 -0500

    Templete format 11

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
+<html lang="es">
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -28,7 +29,7 @@
     <meta name="description" content="Premium Quality and Responsive UI for Dashboard.">
     <meta name="author" content="ThemePixels">
 
-    <title>Bracket Responsive Bootstrap 4 Admin Template</title>
+    <title>Blue developers::Access</title>
 
     <!-- vendor css -->
     <link href="public/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
@@ -36,32 +37,30 @@
 
     <!-- Bracket CSS -->
     <link rel="stylesheet" href="public/css/bracket.css">
-  </head>
+</head>
 
-  <body>
+<body>
 
     <div class="d-flex align-items-center justify-content-center bg-br-primary ht-100v">
 
-      <div class="login-wrapper wd-300 wd-xs-350 pd-25 pd-xs-40 bg-white rounded shadow-base">
-        <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal">[</span> bracket <span class="tx-normal">]</span></div>
-        <div class="tx-center mg-b-60">The Admin Template For Perfectionist</div>
+        <div class="login-wrapper wd-300 wd-xs-350 pd-25 pd-xs-40 bg-white rounded shadow-base">
+            <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal">[</span> Blue developers <span class="tx-normal">]</span></div>
+            <div class="tx-center mg-b-30">Aplicación para la generación de certificados</div>
 
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Enter your username">
-        </div><!-- form-group -->
-        <div class="form-group">
-          <input type="password" class="form-control" placeholder="Enter your password">
-          <a href="" class="tx-info tx-12 d-block mg-t-10">Forgot password?</a>
-        </div><!-- form-group -->
-        <button type="submit" class="btn btn-info btn-block">Sign In</button>
-
-        <div class="mg-t-60 tx-center">Not yet a member? <a href="" class="tx-info">Sign Up</a></div>
-      </div><!-- login-wrapper -->
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Usuario">
+            </div><!-- form-group -->
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="Contraseña">
+            </div><!-- form-group -->
+            <button type="submit" class="btn btn-info btn-block">Iniciar sesión</button>
+        </div><!-- login-wrapper -->
     </div><!-- d-flex -->
 
     <script src="../public/jquery/jquery.js"></script>
     <script src="../public/popper.js/popper.js"></script>
     <script src="../public/bootstrap/bootstrap.js"></script>
 
-  </body>
+</body>
+
 </html>
---
+++ view/html/MainMenu.php
@@ -0,0 +1,25 @@
+  <div class="br-logo"><a href=""><span>[</span>Blue developers<span>]</span></a></div>
+  <div class="br-sideleft overflow-y-auto">
+      <label class="sidebar-label pd-x-15 mg-t-20">Menú</label>
+      <div class="br-sideleft-menu">
+          <a href="index.html" class="br-menu-link">
+              <div class="br-menu-item">
+                  <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
+                  <span class="menu-item-label">Inicio</span>
+              </div><!-- menu-item -->
+          </a><!-- br-menu-link -->
+          <a href="mailbox.html" class="br-menu-link">
+              <div class="br-menu-item">
+                  <i class="menu-item-icon icon ion-ios-book-outline tx-24"></i>
+                  <span class="menu-item-label">Mis cursos</span>
+              </div><!-- menu-item -->
+          </a><!-- br-menu-link -->
+          <a href="card-dashboard.html" class="br-menu-link">
+              <div class="br-menu-item">
+                  <i class="menu-item-icon icon ion-ios-people-outline tx-20"></i>
+                  <span class="menu-item-label">Perfíl</span>
+              </div><!-- menu-item -->
+          </a><!-- br-menu-link -->
+
+      </div>
+  </div>
