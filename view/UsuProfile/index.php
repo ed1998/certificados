9678f83b533a65cde436b5f153b3b7364f2cbbed
Jhon Gutierrez <jhongtrrz@gmail.com>
2021-10-04 21:41:44 -0500

    04/10/21 advance

--- view/UsuProfile/index.php
+++ view/UsuProfile/index.php
@@ -1,1 +1,36 @@
-<h1>Profile</h1>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <?php require_once("../html/MainHead.php") ?>
+    <title>Perfil</title>
+</head>
+
+<body>
+
+    <?php require_once("../html/MainMenu.php"); ?>
+
+    <?php require_once("../html/MainHeader.php"); ?>
+
+    <div class="br-mainpanel">
+        <div class="br-pageheader pd-y-15 pd-l-20">
+            <nav class="breadcrumb pd-0 mg-0 tx-12">
+                <a class="breadcrumb-item" href="#">Perfil</a>
+            </nav>
+        </div><!-- br-pageheader -->
+        <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
+            <h4 class="tx-gray-800 mg-b-5">Perfil</h4>
+            <p class="mg-b-0">Pantalla Perfil</p>
+        </div>
+
+        <div class="br-pagebody">
+
+            <!-- start you own content here -->
+
+        </div><!-- br-pagebody -->
+
+    </div>
+    <?php require_once("../html/MainJs.php") ?>
+
+</body>
+</html>
--- view/html/MainMenu.php
+++ view/html/MainMenu.php
@@ -1,25 +1,30 @@
-  <div class="br-logo"><a href=""><span>[</span>Blue developers<span>]</span></a></div>
+  <div class="br-logo"><a href="../UsuHome/"><span>[</span>Blue developers<span>]</span></a></div>
   <div class="br-sideleft overflow-y-auto">
       <label class="sidebar-label pd-x-15 mg-t-20">Menú</label>
       <div class="br-sideleft-menu">
-          <a href="index.html" class="br-menu-link">
+          <a href="../UsuHome/" class="br-menu-link">
               <div class="br-menu-item">
                   <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
                   <span class="menu-item-label">Inicio</span>
               </div><!-- menu-item -->
           </a><!-- br-menu-link -->
-          <a href="mailbox.html" class="br-menu-link">
+          <a href="../UsuCourse/" class="br-menu-link">
               <div class="br-menu-item">
                   <i class="menu-item-icon icon ion-ios-book-outline tx-24"></i>
                   <span class="menu-item-label">Mis cursos</span>
               </div><!-- menu-item -->
           </a><!-- br-menu-link -->
-          <a href="card-dashboard.html" class="br-menu-link">
+          <a href="../UsuProfile/" class="br-menu-link">
               <div class="br-menu-item">
                   <i class="menu-item-icon icon ion-ios-people-outline tx-20"></i>
                   <span class="menu-item-label">Perfíl</span>
               </div><!-- menu-item -->
           </a><!-- br-menu-link -->
-
+          <a href="#" class="br-menu-link">
+              <div class="br-menu-item">
+                  <i class="menu-item-icon icon ion-power tx-20"></i>
+                  <span class="menu-item-label">Cerrar sesion</span>
+              </div><!-- menu-item -->
+          </a><!-- br-menu-link -->
       </div>
   </div>
